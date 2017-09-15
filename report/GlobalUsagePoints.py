d5ad7ff073a08dc1cb80c8cf0b8a3d55e6c14bef
Kaoru Fukumoto <k1fukumoto@gmail.com>
2017-09-15 10:26:58 +0900

    clean-up obsolete comments

--- report/GlobalUsagePoints.py
+++ report/GlobalUsagePoints.py
@@ -9,14 +9,9 @@ try:
     df_raw
 except NameError:
     df_raw = pd.read_csv(CSVDATA,encoding="ISO-8859-1",parse_dates=['Usage Period'])
-    df_raw.info()
+    df_raw.loc[df_raw['Partner Group Name'] == 'Dimension Data','Partner Group Name'] = 'NTT'
 
-#g = df_raw.groupby('Usage Period')['Value (USD)'].sum()
-#g.plot()
-#plt.bar(g.index,g.values,10,label='MRR')
-#ph.format_trend_chart()
-#plt.show()
-#
-df_raw.loc[df_raw['Partner Group Name'] == 'Dimension Data','Partner Group Name'] = 'NTT'
+df_raw.groupby('Usage Period')['Value (USD)'].sum().plot()
+
 df = df_raw[(df_raw['Usage Period']<=cfg.LAST_QUARTER[2]) & (df_raw['Usage Period']>=cfg.PREV_QUARTER[0])]
 
